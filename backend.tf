1a51ff179993cec253ac720a52359a8f3978cac3
srikanthsrinivasan <srikanth.srinivasa@gmail.com>
2025-08-23 15:46:58 -0500

    pushing storage account

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstate1620sri"  # Replace with your actual name, e.g., tfstate1620sri
+    container_name       = "tfstate-container"
+    key                  = "terraform.tfstate"
+  }
+}
