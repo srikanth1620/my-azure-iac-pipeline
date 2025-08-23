baacdd596f1c92c70620a1c318d4f534910376e3
srikanthsrinivasan <srikanth.srinivasa@gmail.com>
2025-08-23 16:33:42 -0500

    test

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstate1620srigit "  # Replace with your actual name, e.g., tfstate1620sri
+    storage_account_name = "tfstate1620sri"  # Replace with your actual name, e.g., tfstate1620sri
     container_name       = "tfstate-container"
     key                  = "terraform.tfstate"
   }
