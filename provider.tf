abb561574add3b02f0cc0f9600d79d54d8d6d93d
srikanthsrinivasan <srikanth.srinivasa@gmail.com>
2025-08-24 19:26:59 -0500

    1000

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.29"
+      version = ">= 3.29.0"  # Ensure at least 3.29.0
     }
   }
 }
