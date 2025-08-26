c5b4b1baf3847840a3106e7d9dafc5afae9be67e
srikanthsrinivasan <srikanth.srinivasa@gmail.com>
2025-08-25 20:38:29 -0500

    test 1000

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
     # test 100
-    resource "azurerm_resource_group" "second_rg" {
+resource "azurerm_resource_group" "second_rg" {
     name     = var.second_rg_name
     location = var.location
   }
@@ -20,7 +20,6 @@
     tenant_id                   = var.tenant_id
     enable_rbac_authorization   = true
     purge_protection_enabled    = true
-    soft_delete_retention_in_days = 7
   }
 
   # Key Vault Key for CMK
