65559088f122e39dd8743b6cbedcf251d1dab6dc
srikanthsrinivasan <srikanth.srinivasa@gmail.com>
2025-08-25 20:02:10 -0500

    test 1000

--- variable.tf
+++ variable.tf
@@ -1,29 +1,29 @@
   variable "second_rg_name" {
-    description = "Name of the second resource group"
-    default     = "second-storage-rg"
-  }
+       description = "Name of the second resource group"
+       default     = "second-storage-rg"
+     }
 
-  variable "location" {
-    description = "Azure region"
-    default     = "eastus"
-  }
+     variable "location" {
+       description = "Azure region"
+       default     = "eastus"
+     }
 
-  variable "tenant_id" {
-    description = "Azure tenant ID"
-    default     = "550420cd-a4f2-4642-941d-ec8d931bcceb"
-  }
+     variable "tenant_id" {
+       description = "Azure tenant ID"
+       default     = "550420cd-a4f2-4642-941d-ec8d931bcceb"
+     }
 
-  variable "cmk_storage_name" {
-    description = "Name of the CMK storage account"
-    default     = "cmkstorage1620sri"
-  }
+     variable "cmk_storage_name" {
+       description = "Name of the CMK storage account"
+       default     = "cmkstorage1620sri"
+     }
 
-  variable "cmk_container_name" {
-    description = "Name of the CMK Blob container"
-    default     = "cmk-container"
-  }
+     variable "cmk_container_name" {
+       description = "Name of the CMK Blob container"
+       default     = "cmk-container"
+     }
 
-  variable "key_vault_name" {
-    description = "Name of the Key Vault for CMK"
-    default     = "cmkvault1620sri"
-  }
+     variable "key_vault_name" {
+       description = "Name of the Key Vault for CMK"
+       default     = "cmkvault1620sri"
+     }
