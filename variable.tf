26d42311fdfe86ebd6fc6d089cad632cefa17f58
srikanthsrinivasan <srikanth.srinivasa@gmail.com>
2025-08-25 20:50:27 -0500

    test 1000

--- variable.tf
+++ variable.tf
@@ -1,39 +1,39 @@
 variable "second_rg_name" {
-      description = "Name of the second resource group"
-      default     = "second-storage-rg"
-    }
+    description = "Name of the second resource group"
+    default     = "second-storage-rg"
+  }
 
-    variable "location" {
-      description = "Azure region"
-      default     = "eastus"
-    }
+  variable "location" {
+    description = "Azure region"
+    default     = "eastus"
+  }
 
-    variable "tenant_id" {
-      description = "Azure tenant ID"
-      default     = "550420cd-a4f2-4642-941d-ec8d931bcceb"
-    }
+  variable "tenant_id" {
+    description = "Azure tenant ID"
+    default     = "550420cd-a4f2-4642-941d-ec8d931bcceb"
+  }
 
-    variable "cmk_storage_name" {
-      description = "Name of the CMK storage account"
-      default     = "cmkstorage1620sri"
-    }
+  variable "cmk_storage_name" {
+    description = "Name of the CMK storage account"
+    default     = "cmkstorage1620sri"
+  }
 
-    variable "cmk_container_name" {
-      description = "Name of the CMK Blob container"
-      default     = "cmk-container"
-    }
+  variable "cmk_container_name" {
+    description = "Name of the CMK Blob container"
+    default     = "cmk-container"
+  }
 
-    variable "key_vault_name" {
-      description = "Name of the Key Vault for CMK"
-      default     = "cmkvault1620sri"
-    }
+  variable "key_vault_name" {
+    description = "Name of the Key Vault for CMK"
+    default     = "cmkvault1620sri"
+  }
 
-    variable "cmk_identity_name" {
-      description = "Name of the user-assigned managed identity for CMK"
-      default     = "cmkidentity1620sri"
-    }
+  variable "cmk_identity_name" {
+    description = "Name of the user-assigned managed identity for CMK"
+    default     = "cmkidentity1620sri"
+  }
 
-    variable "service_principal_id" {
-      description = "Object ID of the Terraform service principal"
-      default     = "88ce2d43-0395-49a0-b773-7f89cfc69384"
-    }
+  variable "service_principal_id" {
+    description = "Object ID of the Terraform service principal"
+    default     = "88ce2d43-0395-49a0-b773-7f89cfc69384"
+  }
