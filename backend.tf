ad1fdaf47bf5a134f39450d93dd849bc06ae6246
srikanthsrinivasan <srikanth.srinivasa@gmail.com>
2026-04-11 17:57:02 -0500

    testing

--- backend.tf
+++ backend.tf
@@ -9,13 +9,9 @@ resource "azurerm_storage_account" "tfstate" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  # Required by most "RequireStorageAccountEncryption" policies
+  # Minimal settings - should work now that policy is removed
   min_tls_version                  = "TLS1_2"
   allow_nested_items_to_be_public  = false
-  infrastructure_encryption_enabled = true
-
-  # This argument was removed in newer AzureRM provider versions
-  # https_traffic_only_enabled is now default behavior, so we removed it
 }
 
 resource "azurerm_storage_container" "tfstate" {
