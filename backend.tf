aaf00b0a83e834fe53a2ae8b59f969ac21cc929f
srikanthsrinivasan <srikanth.srinivasa@gmail.com>
2026-04-11 18:25:21 -0500

    testing

--- backend.tf
+++ backend.tf
@@ -1,17 +1,9 @@
-# backend.tf - Using existing storage account (no creation)
-
-data "azurerm_resource_group" "tfstate" {
-  name = "tfstate-rg"
-}
-
-# Read the existing storage account (do not create it again)
-data "azurerm_storage_account" "tfstate" {
-  name                = "tfstate1620sri"
-  resource_group_name = "tfstate-rg"
-}
-
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate-container"
-  storage_account_id    = data.azurerm_storage_account.tfstate.id     # Use storage_account_id (new recommended way)
-  container_access_type = "private"
+# backend.tf
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstate1620sri"
+    container_name       = "tfstate-container"
+    key                  = "terraform.tfstate"
+  }
 }
--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# main.tf - Clean starting point
+# main.tf
 
 terraform {
   required_providers {
@@ -9,19 +9,27 @@ terraform {
   }
 }
 
-# Use existing Resource Group for tfstate
-data "azurerm_resource_group" "tfstate" {
-  name = "tfstate-rg"
+# 1. Create Resource Group
+resource "azurerm_resource_group" "tfstate" {
+  name     = "tfstate-rg"
+  location = "East US 2"
 }
 
-# Read existing storage account
-data "azurerm_storage_account" "tfstate" {
-  name                = "tfstate1620sri"
-  resource_group_name = "tfstate-rg"
+# 2. Create Storage Account
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "tfstate1620sri"
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  min_tls_version                 = "TLS1_2"
+  allow_nested_items_to_be_public = false
 }
 
-# Read existing container (do not create it again)
-data "azurerm_storage_container" "tfstate" {
+# 3. Create Container
+resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate-container"
-  storage_account_name  = "tfstate1620sri"
+  storage_account_id    = azurerm_storage_account.tfstate.id
+  container_access_type = "private"
 }
