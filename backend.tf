6809293963920e4783f99d1ba4ef07ff29c8a97e
srikanthsrinivasan <srikanth.srinivasa@gmail.com>
2026-04-11 17:50:12 -0500

    testing

--- backend.tf
+++ backend.tf
@@ -1,17 +1,21 @@
 data "azurerm_resource_group" "tfstate" {
-  name = "tfstate-rg"   # ← Change only if your RG name is different
+  name = "tfstate-rg"
 }
 
 resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstate1620sri"     # Must be globally unique and lowercase
+  name                     = "tfstate1620sri"     # Must be globally unique + lowercase
   resource_group_name      = data.azurerm_resource_group.tfstate.name
   location                 = data.azurerm_resource_group.tfstate.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  # Modern way - HTTPS traffic only is now default, so we use min_tls_version instead
-  min_tls_version          = "TLS1_2"
-  allow_nested_items_to_be_public = false
+  # These settings are usually required by "RequireStorageAccountEncryption" policy
+  min_tls_version                  = "TLS1_2"
+  allow_nested_items_to_be_public  = false
+  infrastructure_encryption_enabled = true   # ← This is the key setting
+
+  # Enable HTTPS only (modern way)
+  https_traffic_only_enabled = true
 }
 
 resource "azurerm_storage_container" "tfstate" {
