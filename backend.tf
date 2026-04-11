a87ea047f4902ce518b4699a39287ffabb383e21
srikanthsrinivasan <srikanth.srinivasa@gmail.com>
2026-04-11 17:53:38 -0500

    testing

--- backend.tf
+++ backend.tf
@@ -3,19 +3,19 @@ data "azurerm_resource_group" "tfstate" {
 }
 
 resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstate1620sri"     # Must be globally unique + lowercase
+  name                     = "tfstate1620sri"     # Must be globally unique and all lowercase
   resource_group_name      = data.azurerm_resource_group.tfstate.name
   location                 = data.azurerm_resource_group.tfstate.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  # These settings are usually required by "RequireStorageAccountEncryption" policy
+  # Required by most "RequireStorageAccountEncryption" policies
   min_tls_version                  = "TLS1_2"
   allow_nested_items_to_be_public  = false
-  infrastructure_encryption_enabled = true   # ← This is the key setting
+  infrastructure_encryption_enabled = true
 
-  # Enable HTTPS only (modern way)
-  https_traffic_only_enabled = true
+  # This argument was removed in newer AzureRM provider versions
+  # https_traffic_only_enabled is now default behavior, so we removed it
 }
 
 resource "azurerm_storage_container" "tfstate" {
